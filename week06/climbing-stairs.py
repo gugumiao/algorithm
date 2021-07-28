c5d6d15f83498aa78c98d2dee19038cda9b912f7
gugumiao <gu@gugumiaomiao.com>
2021-07-28 19:56:43 +0800

    week06

---
+++ week06/climbing-stairs.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a = b = 1
+        for i in range(2, n + 1):
+            a, b = b, a + b
+        return b
+
