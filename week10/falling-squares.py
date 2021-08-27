190763c8ea0b5d36b2b28b7eef21bf02896aa550
gugumiao <gu@gugumiaomiao.com>
2021-08-27 22:28:57 +0800

    week10

--- week10/falling-squares.py
+++ week10/falling-squares.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+class Solution:
+    def fallingSquares(self, positions: List[List[int]]) -> List[int]:
+        s = set()
+
+        for pos in positions:
+            s.add(pos[0])
+            s.add(pos[0] + pos[1] - 1)
+
+        s = sorted(list(s))
+        smap = {val: idx for idx, val in enumerate(s)}
+        ans = []
+        res = 0
+        height = [0] * len(s)
+
+        for pos in positions:
+            x, y = smap[pos[0]], smap[pos[0] + pos[1] - 1]
+            max_cur = max(height[x: y+1])
+            cur_res = max_cur + pos[1]
+
+            for idx in range(x, y+1):
+                height[idx] = cur_res
+
+            res = max(cur_res, res)
+            ans.append(res)
+        return ans
 
